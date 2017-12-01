1cd1ce3f941939ac25b166fd076e00f870113a4b
alsun <aminabdo8@gmail.com>
2017-12-02 00:03:49 +0200

    navbar and part of intro

---
+++ js/plugins.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:4,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+});
